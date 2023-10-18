9bbbbb13d0ed33de090160dd88443c815c9415e2
Metty Kapgen <mett.kapgen@hotmail.com>
2023-10-18 13:20:29 +0200

    Workmanager able to talk and send back to api

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,35 +1,28 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:io';
-import 'dart:math';
 
 import 'package:app/Coord.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:workmanager/workmanager.dart';
 import "package:http/http.dart" as http;
 
-Future<void> main() async {
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
   runApp(MaterialApp(
     home: Home(),
   ));
 }
 
-const simpleTaskKey = "be.tramckrijte.workmanagerExample.simpleTask";
-const rescheduledTaskKey = "be.tramckrijte.workmanagerExample.rescheduledTask";
-const failedTaskKey = "be.tramckrijte.workmanagerExample.failedTask";
-const simpleDelayedTask = "be.tramckrijte.workmanagerExample.simpleDelayedTask";
-const simplePeriodicTask =
-    "be.tramckrijte.workmanagerExample.simplePeriodicTask";
-const simplePeriodic1HourTask =
-    "be.tramckrijte.workmanagerExample.simplePeriodic1HourTask";
+const simpleTaskKey = "workmanager.simpleTask";
+const simplePeriodicTask = "workmanager.simplePeriodicTask";
+const simplePeriodic1HourTask = "workmanager.simplePeriodic1HourTask";
 
 void _backgroundTask() async {
   //SendPort sendPort) async {
   // Perform time-consuming operation here
-  //BackgroundIsolateBinaryMessenger.ensureInitialized();
   //Position pos = await Geolocator.getCurrentPosition(
   //    desiredAccuracy: LocationAccuracy.high);
   final response = await http.get(Uri.parse("http://10.0.2.2:5000/"));
@@ -79,6 +72,7 @@ void callbackDispatcher() {
         if (response.statusCode == 200) {
           // If the server did return a 200 OK response,
           // then parse the JSON.
+          print("Response Status 200");
           final parsedJson = jsonDecode(response.body);
           final tmp = Coord.fromJson(parsedJson);
           final newCordx = tmp.Lat + 3; //pos.latitude;
@@ -98,7 +92,7 @@ void callbackDispatcher() {
           if (response2.statusCode == 201) {
             // If the server did return a 201 OK response,
             // then parse the JSON.
-            print("Successful Post");
+            print("Response Status 201");
           } else {
             // If the server did not return a 200 OK response,
             // then throw an exception.
@@ -114,36 +108,12 @@ void callbackDispatcher() {
         prefs.setBool("test", true);
         print("Bool from prefs: ${prefs.getBool("test")}");
         break;
-      case rescheduledTaskKey:
-        final key = inputData!['key']!;
-        final prefs = await SharedPreferences.getInstance();
-        if (prefs.containsKey('unique-$key')) {
-          print('has been running before, task is successful');
-          return true;
-        } else {
-          await prefs.setBool('unique-$key', true);
-          print('reschedule task');
-          return false;
-        }
-      case failedTaskKey:
-        print('failed task');
-        return Future.error('failed');
-      case simpleDelayedTask:
-        print("$simpleDelayedTask was executed");
-        break;
       case simplePeriodicTask:
         _backgroundTask();
         print("$simplePeriodicTask was executed");
         break;
       case simplePeriodic1HourTask:
         print("$simplePeriodic1HourTask was executed");
-        break;
-      case Workmanager.iOSBackgroundTask:
-        print("The iOS background fetch was triggered");
-        Directory? tempDir = await getTemporaryDirectory();
-        String? tempPath = tempDir.path;
-        print(
-            "You can access other plugins in the background, for example Directory.getTemporaryDirectory(): $tempPath");
         break;
     }
 
@@ -160,7 +130,6 @@ class _HomeState extends State<Home> {
   @override
   void initState() {
     super.initState();
-    //_startBackgroundTask();
   }
 
   List<Coord> apiResponse = [];
@@ -299,133 +268,3 @@ class _HomeState extends State<Home> {
     );
   }
 }
-
-/*
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Flutter WorkManager Example"),
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                Text(
-                  "Plugin initialization",
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                ElevatedButton(
-                  child: Text("Start the Flutter background service"),
-                  onPressed: () {
-                    Workmanager().initialize(
-                      callbackDispatcher,
-                      isInDebugMode: true,
-                    );
-                  },
-                ),
-                SizedBox(height: 16),
-
-                //This task runs once.
-                //Most likely this will trigger immediately
-                ElevatedButton(
-                  child: Text("Register OneOff Task"),
-                  onPressed: () {
-                    Workmanager().registerOneOffTask(
-                      simpleTaskKey,
-                      simpleTaskKey,
-                      inputData: <String, dynamic>{
-                        'int': 1,
-                        'bool': true,
-                        'double': 1.0,
-                        'string': 'string',
-                        'array': [1, 2, 3],
-                      },
-                    );
-                  },
-                ),
-                ElevatedButton(
-                  child: Text("Register rescheduled Task"),
-                  onPressed: () {
-                    Workmanager().registerOneOffTask(
-                      rescheduledTaskKey,
-                      rescheduledTaskKey,
-                      inputData: <String, dynamic>{
-                        'key': Random().nextInt(64000),
-                      },
-                    );
-                  },
-                ),
-                ElevatedButton(
-                  child: Text("Register failed Task"),
-                  onPressed: () {
-                    Workmanager().registerOneOffTask(
-                      failedTaskKey,
-                      failedTaskKey,
-                    );
-                  },
-                ),
-                //This task runs once
-                //This wait at least 10 seconds before running
-                ElevatedButton(
-                    child: Text("Register Delayed OneOff Task"),
-                    onPressed: () {
-                      Workmanager().registerOneOffTask(
-                        simpleDelayedTask,
-                        simpleDelayedTask,
-                        initialDelay: Duration(seconds: 10),
-                      );
-                    }),
-                SizedBox(height: 8),
-                //This task runs periodically
-                //It will wait at least 10 seconds before its first launch
-                //Since we have not provided a frequency it will be the default 15 minutes
-                ElevatedButton(
-                    child: Text("Register Periodic Task (Android)"),
-                    onPressed: Platform.isAndroid
-                        ? () {
-                            Workmanager().registerPeriodicTask(
-                              simplePeriodicTask,
-                              simplePeriodicTask,
-                              initialDelay: Duration(seconds: 10),
-                            );
-                          }
-                        : null),
-                //This task runs periodically
-                //It will run about every hour
-                ElevatedButton(
-                    child: Text("Register 15 min Periodic Task (Android)"),
-                    onPressed: Platform.isAndroid
-                        ? () {
-                            Workmanager().registerPeriodicTask(
-                              simplePeriodicTask,
-                              simplePeriodic1HourTask,
-                              frequency: Duration(minutes: 15),
-                            );
-                          }
-                        : null),
-                SizedBox(height: 16),
-                Text(
-                  "Task cancellation",
-                  style: Theme.of(context).textTheme.headlineSmall,
-                ),
-                ElevatedButton(
-                  child: Text("Cancel All"),
-                  onPressed: () async {
-                    await Workmanager().cancelAll();
-                    print('Cancel all tasks completed');
-                  },
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-}
-*/
